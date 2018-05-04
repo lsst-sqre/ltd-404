0bf976344f84f25479058def6686d73913202f9c
Jonathan Sick <jsick@lsst.org>
2018-05-04 14:23:17 -0700

    Add gulp sass task to compile sass
    Uses:
    
    - gulp-sass
    - gulp-sourcemaps
    - gulp-clean-css

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,19 +1,61 @@
+import cleanCSS from 'gulp-clean-css';
 import gulp from 'gulp';
 import minimist from 'minimist';
+import sass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
 
 // parse command line options
 // [--env dev (default) | prod]
 const options = minimist(process.argv);
 const env = options.env || 'dev';
 
+// Environment-based configurations for CleanCss
+// https://github.com/jakubpawlowicz/clean-css
+const cleanCssConfig = {
+  dev: {
+    compatibility: '*',
+    level: 2,
+    format: 'beautify'
+  },
+
+  prod: {
+    compatibility: '*',
+    level: 2,
+  }
+};
+
 /*
  * gulp environment
  * Prints the environment setting.
-*/
+ */
 export const environment = () => console.log(`${env}`);
+
+/*
+ * gulp sass
+ * Compile the sass
+ */
+const sassTask = () => {
+  let stream = gulp.src('styles.scss')
+    // Initialize sourcemaps
+    .pipe(sourcemaps.init())
+    // Compile sass synchronously
+    .pipe(sass.sync().on('error', sass.logError))
+    // Clean CSS
+    .pipe(cleanCSS(cleanCssConfig[env]));
+
+  if (env === 'dev') {
+    // Write out sourcemaps
+    stream.pipe(sourcemaps.write());
+  }
+
+  stream.pipe(gulp.dest('.'));
+  return stream;
+
+};
+gulp.task('sass', sassTask);
 
 /*
  * gulp
  * Default task.
-*/
+ */
 export default environment;
