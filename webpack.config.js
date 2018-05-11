fe7ecbf1ef30726fcdb0ad3e2dd1f5364e3964fa
Jonathan Sick <jsick@lsst.org>
2018-05-11 11:12:19 -0700

    Run babel via webpack
    This down-compiles ES6 javascript to ES5.

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
   output: {
     filename: 'app.bundle.js',
     path: path.resolve(__dirname)
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+          }
+        }
+      }
+    ]
   }
 };
